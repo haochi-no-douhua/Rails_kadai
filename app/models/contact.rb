328fbff9515c38d74cc9e2c04c7ece2f8d4ad2b4
sara <haochi_no_douhua@cpe-172-101-51-232.maine.res.rr.com>
2020-10-15 15:52:51 +0900

    kadai01

---
+++ app/models/contact.rb
@@ -0,0 +1,8 @@
+class Contact < ApplicationRecord
+  validates :name, presence: true,
+             length: { maximum: 140 }
+  validates :email, presence: true,
+             length: { maximum: 140 }
+  validates :content, presence: true,
+             length: { maximum: 140 }
+end
